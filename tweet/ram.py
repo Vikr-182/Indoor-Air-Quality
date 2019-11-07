d6a4012ae627459fb32721858d70520b99c1cd3f
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2019-11-08 00:22:45 +0530

    added twitter-code

---
+++ tweet/ram.py
@@ -0,0 +1,41 @@
+#import tweepy
+from TwitterAPI import TwitterAPI
+import matplotlib.pyplot as plt
+import csv
+import os
+import requests
+
+URL = "http://api.thingspeak.com/channels/864235/feed.csv?results=8000"
+download = requests.get(URL).content.decode('utf-8')#.split("\n",1)[1]
+#print(download.split(','))
+
+jj=0
+dict1={}
+dict2={}
+dict3={}
+for i in download.split('\n'):
+    print(i.split(','))
+    j = i.split(',')
+    dict1[j[0]] = j[1]
+    dict2[j[0]] = j[2]
+    dict3[j[0]] = j[3]
+    print(dict1[j[0]])
+    jj = jj + 1
+    if jj > 5:
+        break;
+
+
+'''
+consumer_key = 'TynhLBXGxZepWH0WQVf26QdpV'
+consumer_secret = 'n7hBsbrQXUTmRgps9pmPYNl85OKy3qyHd15fOZ7W7TD6lM3Rek'
+access_token = '1192421889815105536-B6osx8nZSbh6oi7ORt5ydybBZMVpSL'
+access_token_secret = 'se2unVv40EJajV3CJMYJnhrFQ43zcJP4ncPX5DtBrQVgI'
+
+api = TwitterAPI(consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret)
+
+r = api.request('statuses/update', {'status': str(data)})
+print( 'SUCCESS' if r.status_code == 200 else 'FAILURE' )
+'''
